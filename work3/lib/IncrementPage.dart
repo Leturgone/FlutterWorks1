e81ca58342c6765b2c1feece3a1ab37fde48c6f7
Leturgone <finekon221@gmail.com>
2025-09-27 02:07:53 +0300

    Реализован виджет с кнопкой инкремента

--- work3/lib/IncrementPage.dart
+++ work3/lib/IncrementPage.dart
@@ -1,1 +1,27 @@
 
+import 'package:flutter/material.dart';
+
+class IncrementPage extends StatelessWidget{
+  final int counter;
+  final VoidCallback onIncrement;
+
+  const IncrementPage({super.key, required this.counter, required this.onIncrement});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        color: Colors.tealAccent,
+        padding: EdgeInsets.all(80.0),
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(backgroundColor: Colors.white),
+          onPressed: onIncrement,
+          child: Text(
+            style: TextStyle(color: Colors.black),
+              'Увеличить счетчик на 10 (Текущий: $counter)'
+          ),
+        ),
+      )
+    );
+  }
+}
