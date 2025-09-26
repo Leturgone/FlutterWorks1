d28afe817c4fca4e5f5e223d5ffeeac88169de67
Leturgone <finekon221@gmail.com>
2025-09-27 02:30:58 +0300

    Реализован виджет со степенями

--- work3/lib/SquarePage.dart
+++ work3/lib/SquarePage.dart
@@ -1,1 +1,33 @@
 
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class SquarePage extends StatelessWidget{
+  final int counter;
+
+  const SquarePage({super.key, required this.counter});
+
+  @override
+  Widget build(BuildContext context) {
+    int square = pow(counter, 2).toInt();
+    int cube = pow(counter, 3).toInt();
+    return Center(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                'Квадрат счетчика: $square',
+                style: TextStyle(fontSize: 24),
+              ),
+              Text(
+                'Куб счетчика: $cube',
+                style: TextStyle(fontSize: 20),
+              ),
+            ]
+        )
+    );
+  }
+
+}
