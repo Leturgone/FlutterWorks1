a2f7818a5d06e0aab2c88cb0ad479f284575d108
Leturgone <finekon221@gmail.com>
2025-09-27 02:47:55 +0300

    Добавлен мэйн скрин в мейн

--- work3/lib/main.dart
+++ work3/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:work3/MainScreen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MainScreen(),
     );
   }
 }
