54164e5c031fb6a0e64261fea6cd9f20eb825eef
Leturgone <finekon221@gmail.com>
2025-09-27 01:31:21 +0300

    Реализован виджет счетчика

--- work3/lib/CounterPage.dart
+++ work3/lib/CounterPage.dart
@@ -1,1 +1,17 @@
 
+import 'package:flutter/material.dart';
+
+class CounterPage extends StatelessWidget{
+  final int counter;
+  const CounterPage({super.key, required this.counter});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'Текущий счетчик: $counter',
+        style: TextStyle(fontSize: 32, color: Colors.green),
+      ),
+    );
+  }
+}
