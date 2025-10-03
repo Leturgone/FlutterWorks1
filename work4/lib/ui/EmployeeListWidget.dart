55c1669235b3fa9b3e771a1780ea5e2bb971e32e
Leturgone <finekon221@gmail.com>
2025-10-04 02:15:01 +0300

    Сделано добавление элемента в список сотрудников

--- work4/lib/ui/EmployeeListWidget.dart
+++ work4/lib/ui/EmployeeListWidget.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 
 // Экран с сотрудиками (использует ListView.separated )
@@ -12,12 +13,18 @@ class EmployeeScreen extends StatefulWidget {
 class _EmployeeScreenState extends State<EmployeeScreen> {
   List<String> employees = List.generate(40, (index) => 'Сотрудник ${index + 1}');
 
+  int counter = 40;
+
   @override
   Widget build(BuildContext context) {
     return ListView.separated(
       itemBuilder: (_,position) => GestureDetector(
           key: ValueKey(employees[position]),
-          onTap: () => setState(() => employees.removeAt(position)),
+          onDoubleTap: () => setState(() => employees.add('Сотрудник ${counter = counter+1}')),
+          onTap: () => setState(() {
+            employees.removeAt(position);
+            counter--;
+          }),
           child: Text(employees[position])),
       separatorBuilder: (_,_) => const Divider(),
       itemCount: employees.length,
