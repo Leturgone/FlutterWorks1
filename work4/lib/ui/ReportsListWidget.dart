ca667cd748989072007e246161a2036ba3704bb5
Leturgone <finekon221@gmail.com>
2025-10-04 02:15:15 +0300

    Сделано добавление элемента в список отчетов

--- work4/lib/ui/ReportsListWidget.dart
+++ work4/lib/ui/ReportsListWidget.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 
 // Экран с отчетами (использует ListView)
@@ -11,13 +12,17 @@ class ReportsScreen extends StatefulWidget {
 
 class _ReportsScreenState extends State<ReportsScreen> {
   List<String> reports = List.generate(40, (index) => 'Отчет ${index + 1}');
-
+  int counter = 40;
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemBuilder: (_,position) => GestureDetector(
         key: ValueKey(reports[position]),
-        onTap: () => setState(() => reports.removeAt(position)),
+        onDoubleTap: () => setState(() => reports.add('Отчет ${counter = counter+1}')),
+        onTap: () => setState(() {
+          reports.removeAt(position);
+          counter--;
+        }),
         child: Text(reports[position])),
       itemCount: reports.length,
     );
