f2539a2e618ce3caa856e6238a0ce603567f8ca7
Leturgone <finekon221@gmail.com>
2025-10-11 02:34:03 +0300

    Добавлен виджет элемента списка заметок

---
+++ work5/lib/features/impression_note/widgets/impression_note_list_view.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:work5/features/impression_note/models/impression_note.dart';
+import 'package:work5/features/impression_note/widgets/impression_note_tile.dart';
+
+class ImpressionNoteListView extends StatelessWidget {
+  final List<ImpressionNote> notes;
+  final ValueChanged<int> onDelete;
+  final ValueChanged<ImpressionNote> onEdit;
+  final ValueChanged<ImpressionNote> onNoteTap;
+
+  const ImpressionNoteListView({
+    super.key,
+    required this.notes,
+    required this.onEdit,
+    required this.onDelete,
+    required this.onNoteTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    if (notes.isEmpty) {
+      return const Center(
+        child: Text(
+          'Список заметок о впечатлениях пуст',
+          style: TextStyle(
+            fontSize: 16,
+            color: Colors.grey,
+          ),
+        ),
+      );
+    }
+
+    return ListView.builder(
+      itemCount: notes.length,
+      itemBuilder: (context, index) {
+        final note = notes[index];
+        return ImpressionNoteTile(note: note, onDelete: onDelete, onTap: onNoteTap,onEdit: onEdit);
+      },
+    );
+  }
+}
