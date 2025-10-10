02ae150ddb72db8009b908f9f075e2761747238d
Leturgone <finekon221@gmail.com>
2025-10-11 02:31:21 +0300

    Добавлен экран со списком заметок о впечатлениях

---
+++ work5/lib/features/impression_note/screens/impression_note_list_screen.dart
@@ -0,0 +1,45 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:work5/features/impression_note/models/impression_note.dart';
+import 'package:work5/features/impression_note/widgets/impression_note_list_view.dart';
+
+class ImpressionNoteListScreen extends StatelessWidget {
+  final List<ImpressionNote> impressionNotes;
+  final VoidCallback onAdd;
+  final void Function(int id)? onDelete;
+  final ValueChanged<ImpressionNote> onNoteTap;
+  final ValueChanged<ImpressionNote>  onEdit;
+  final VoidCallback onSort;
+
+  const ImpressionNoteListScreen({
+    super.key, required this.impressionNotes,
+    required this.onAdd, required this.onDelete,
+    required this.onNoteTap, required this.onEdit, required this.onSort});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Заметки о впечатлениях'),
+        ),
+        body: ImpressionNoteListView(notes: impressionNotes, onDelete: onDelete!, onNoteTap: onNoteTap,onEdit: onEdit),
+        floatingActionButton:Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            FloatingActionButton(
+              onPressed: onAdd,
+              tooltip: 'Добавить заметку',
+              child: const Icon(Icons.add),
+            ),
+            SizedBox(width: 16),
+            FloatingActionButton(
+              onPressed: onSort,
+              tooltip: 'Сортировать',
+              child: const Icon(Icons.sort_by_alpha),
+            ),
+          ],
+        ),
+    );
+  }
+}
