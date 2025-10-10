b8b3d1100b8edb5a9f4fdf5e284db14b2eb60107
Leturgone <finekon221@gmail.com>
2025-10-11 02:29:42 +0300

    Добавлен экран о комиксе

---
+++ work5/lib/features/comic_series/screens/comic_series_about_screen.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+import '../models/comic_series.dart';
+
+class ComicSeriesAboutScreen extends StatelessWidget {
+  final ComicSeries series;
+  final VoidCallback onBack;
+
+  const ComicSeriesAboutScreen({Key? key, required this.series, required this.onBack}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.network(
+                series.image,
+                width: 150,
+                height: 150,
+                fit: BoxFit.cover,
+              ),
+              SizedBox(height: 16),
+              Text(
+                series.title,
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 8.0),
+              Text(
+                'Writer: ${series.writer}',
+                style: TextStyle(fontSize: 16, fontStyle: FontStyle.italic),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: Text(
+                  series.description ?? 'Нет описания',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 16),
+                ),
+              ),
+              SizedBox(height: 24),
+              // Кнопка выхода
+              ElevatedButton(
+                onPressed: onBack,
+                child: Text('Назад'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
