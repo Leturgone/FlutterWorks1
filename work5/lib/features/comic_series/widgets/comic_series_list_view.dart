884c37b3b1ad84e7f22da73c20f341b2c7c2aff3
Leturgone <finekon221@gmail.com>
2025-10-11 02:33:32 +0300

    Добавлен виджет списка серий

---
+++ work5/lib/features/comic_series/widgets/comic_series_list_view.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:work5/features/comic_series/widgets/comic_series_tile.dart';
+
+import '../models/comic_series.dart';
+
+
+class ComicSeriesListView extends StatelessWidget {
+  final List<ComicSeries> seriesList;
+  final ValueChanged<ComicSeries> onSeriesTap;
+
+  const ComicSeriesListView({
+    Key? key,
+    required this.seriesList,
+    required this.onSeriesTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    if (seriesList.isEmpty) {
+      return const Center(
+        child: Text(
+          'Список серий пуст',
+          style: TextStyle(
+            fontSize: 16,
+            color: Colors.grey,
+          ),
+        ),
+      );
+    }
+
+    return ListView.builder(
+      itemCount: seriesList.length,
+      itemBuilder: (context, index) {
+        final series = seriesList[index];
+        return ComicSeriesTile(series: series, onTap: onSeriesTap,);
+      },
+    );
+  }
+}
