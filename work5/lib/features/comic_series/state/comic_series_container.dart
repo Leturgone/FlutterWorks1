be5b5f9833c0b90fa54ed5c9f74e8ca10358a346
Leturgone <finekon221@gmail.com>
2025-10-11 02:40:00 +0300

    Добавлен контейнер для серий комиксов

---
+++ work5/lib/features/comic_series/state/comic_series_container.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:work5/features/comic_series/screens/comic_series_about_screen.dart';
+import 'package:work5/features/comic_series/screens/comic_series_list_screen.dart';
+import 'package:work5/features/shared_data.dart';
+
+import '../models/comic_series.dart';
+
+
+enum Screen { list, about}
+
+class ComicSeriesContainer extends StatefulWidget {
+  const ComicSeriesContainer({super.key});
+
+  @override
+  State<ComicSeriesContainer> createState() => _ComicSeriesContainerState();
+}
+
+class _ComicSeriesContainerState extends State<ComicSeriesContainer> {
+  final List<ComicSeries> _seriesList = SharedData.seriesList;
+
+  ComicSeries? _selectedSeries; // выбранный комикс для отображения деталей
+  Screen _currentScreen = Screen.list;
+
+
+  void _showListScreen() {
+    setState(() {
+      _currentScreen = Screen.list;
+    });
+  }
+
+  void _showAboutScreen(ComicSeries comic) {
+    setState(() {
+      _selectedSeries = comic;
+      _currentScreen = Screen.about;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    switch (_currentScreen) {
+      case Screen.list:
+        return ComicSeriesListScreen(
+          seriesList: _seriesList,
+          onSeriesTap: _showAboutScreen,
+        );
+      case Screen.about:
+        if (_selectedSeries == null){
+          return Container();
+        }else{
+          return ComicSeriesAboutScreen(series: _selectedSeries!, onBack: _showListScreen);
+        }
+    }
+  }
+}
